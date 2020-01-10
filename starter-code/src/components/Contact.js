6ec810a1653d26acd546a43500c0bda6e8ee0bb1
raulrequenac <raulrequec@hotmail.com>
2020-01-10 21:07:07 +0100

    second iteration

--- starter-code/src/components/Contact.js
+++ starter-code/src/components/Contact.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react'
-import './ContactList.css'
-import Contacts from '../contacts.json'
+import React from 'react'
+import './Contacts.css'
 
-class Contact extends Component {
-  state = {
-    fiveContacts: Contacts.slice(0,5)
-  }
 
-  render() {
-    return (
-      <div>
-        {this.state.fiveContacts.map((contact) => {
-          return (
-            <div className="row">
-              <img alt="picurl" src={contact.pictureUrl} width="100" className="space"></img>
-              <p className="space">{contact.name}</p>
-              <p className="space">{contact.popularity}</p>
-            </div>
-          )
-        })}
-      </div>
-    )
-  }
-}
+const Contact = ({
+  pictureUrl,
+  name,
+  popularity
+}) => (
+  <div className="Contact">
+    <div className="row">
+      <img alt="picurl" src={pictureUrl} width="100" className="space"></img>
+      <p className="space">{name}</p>
+      <p className="space">{popularity}</p>
+    </div>
+  </div>
+)
 
 export default Contact
--- starter-code/src/components/ContactList.js
+++ starter-code/src/components/ContactList.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react'
-import './ContactList.css'
+import './Contacts.css'
 import Contact from './Contact'
+import contactsData from '../contacts.json'
 
 class ContactList extends Component {
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    this.setState({contacts: contactsData.slice(0,5)})
+  }
+
+  onClickRandomContact() {
+    if (this.state.contacts.length < contactsData.length) {
+      let randomContact = {}
+      do {
+        const randomNumber = Math.floor(Math.random() * contactsData.length)
+        randomContact = contactsData[randomNumber]
+      } while (this.state.contacts.some(contact => (contact.name === randomContact.name)))
+
+      this.setState({
+        contacts: [...this.state.contacts, randomContact]
+      })
+    }
+  }
+
   render() {
+    const contactList = this.state.contacts.map(contact => (
+      <div>
+        <Contact {...contact}/>
+      </div>
+    ))
+
     return (
       <div className="ContactList">
-        <div className="row">
-          <h3 className="space">Picture</h3>
-          <h3 className="space">Name</h3>
-          <h3 className="space">Popularity</h3>
-        </div>
-        <Contact />
+        {contactList.length === 0 ? 'Loading...' : (
+          <div>
+            <button onClick={() => this.onClickRandomContact()}>Add Random Contacts</button>
+            <div className="row">
+              <h3 className="space">Picture</h3>
+              <h3 className="space">Name</h3>
+              <h3 className="space">Popularity</h3>
+            </div>
+            {contactList}
+          </div>
+        )}
       </div>
     )
   }
